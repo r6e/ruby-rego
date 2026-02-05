3476a78363bce80a28e19464cf880f8de869dd52
Rob Trame <me@r6e.dev>
2026-02-04 17:49:08 -0700

    Implement expression parsing

--- lib/ruby/rego/parser.rb
+++ lib/ruby/rego/parser.rb
@@ -10,11 +10,50 @@ module Ruby
     # Parses a token stream into an AST module.
     # rubocop:disable Metrics/ClassLength
     # :reek:TooManyMethods
+    # :reek:TooManyConstants
     class Parser
       IDENTIFIER_TOKEN_TYPES = [TokenType::IDENT, TokenType::DATA, TokenType::INPUT].freeze
       IDENTIFIER_TOKEN_NAMES = {
         TokenType::DATA => "data",
         TokenType::INPUT => "input"
+      }.freeze
+      BINARY_OPERATOR_MAP = {
+        TokenType::ASSIGN => :assign,
+        TokenType::UNIFY => :unify,
+        TokenType::PIPE => :or,
+        TokenType::AMPERSAND => :and,
+        TokenType::EQ => :eq,
+        TokenType::NEQ => :neq,
+        TokenType::LT => :lt,
+        TokenType::LTE => :lte,
+        TokenType::GT => :gt,
+        TokenType::GTE => :gte,
+        TokenType::PLUS => :plus,
+        TokenType::MINUS => :minus,
+        TokenType::STAR => :mult,
+        TokenType::SLASH => :div,
+        TokenType::PERCENT => :mod
+      }.freeze
+      UNARY_OPERATOR_MAP = {
+        TokenType::NOT => :not,
+        TokenType::MINUS => :minus
+      }.freeze
+      PRIMARY_PARSERS = {
+        TokenType::STRING => :parse_string_literal,
+        TokenType::RAW_STRING => :parse_string_literal,
+        TokenType::NUMBER => :parse_number_literal,
+        TokenType::TRUE => :parse_boolean_literal,
+        TokenType::FALSE => :parse_boolean_literal,
+        TokenType::NULL => :parse_null_literal,
+        TokenType::NOT => :parse_unary_expression,
+        TokenType::MINUS => :parse_unary_expression,
+        TokenType::LPAREN => :parse_parenthesized_expression,
+        TokenType::LBRACKET => :parse_array,
+        TokenType::LBRACE => :parse_braced_literal,
+        TokenType::IDENT => :parse_identifier_expression,
+        TokenType::DATA => :parse_identifier_expression,
+        TokenType::INPUT => :parse_identifier_expression,
+        TokenType::UNDERSCORE => :parse_identifier_expression
       }.freeze
       PACKAGE_PATH_TOKEN_TYPES = [TokenType::IDENT].freeze
       IMPORT_PATH_TOKEN_TYPES = IDENTIFIER_TOKEN_TYPES
@@ -133,8 +172,123 @@ module Ruby
         parse_error("Rule parsing not implemented yet.")
       end
 
-      def parse_expression
-        parse_error("Expression parsing not implemented yet.")
+      def parse_expression(precedence = Precedence::LOWEST)
+        parse_infix_expression(parse_primary, precedence)
+      end
+
+      def parse_infix_expression(left, precedence)
+        return left unless infix_operator?(precedence)
+
+        operator_token = advance
+        parse_infix_expression(parse_infix(left, operator_token), precedence)
+      end
+
+      def infix_operator?(precedence)
+        Helpers.precedence_of(current_token.type) > precedence
+      end
+
+      def parse_primary
+        handler = PRIMARY_PARSERS[current_token.type]
+        return send(handler) if handler
+
+        parse_error("Expected expression.")
+      end
+
+      # :reek:FeatureEnvy
+      def parse_infix(left, operator_token)
+        operator_type = operator_token.type
+        operator = BINARY_OPERATOR_MAP.fetch(operator_type) do
+          parse_error("Unsupported operator: #{operator_type}.")
+        end
+
+        right = parse_expression(Helpers.precedence_of(operator_type))
+        AST::BinaryOp.new(operator: operator, left: left, right: right, location: operator_token.location)
+      end
+
+      def parse_reference(base)
+        reference_base, path = Helpers.normalize_reference_base(base)
+        parse_reference_path(path)
+        AST::Reference.new(base: reference_base, path: path, location: base.location)
+      end
+
+      def parse_array
+        start = consume(TokenType::LBRACKET)
+        elements = [] # @type var elements: Array[AST::expression]
+        elements = parse_expression_list_until(TokenType::RBRACKET) unless match?(TokenType::RBRACKET)
+        consume(TokenType::RBRACKET, "Expected ']' after array literal.")
+        AST::ArrayLiteral.new(elements: elements, location: start.location)
+      end
+
+      def parse_object(start_token, first_key)
+        pairs = [] # @type var pairs: Array[[AST::expression, AST::expression]]
+        pairs << parse_object_pair(first_key)
+        append_object_pairs(pairs)
+        consume(TokenType::RBRACE, "Expected '}' after object literal.")
+        AST::ObjectLiteral.new(pairs: pairs, location: start_token.location)
+      end
+
+      def parse_set(start_token, first_element = nil)
+        return empty_set_literal(start_token) if empty_set?(first_element)
+
+        elements = parse_expression_list_until_with_first(TokenType::RBRACE, first_element)
+        consume(TokenType::RBRACE, "Expected '}' after set literal.")
+        AST::SetLiteral.new(elements: elements, location: start_token.location)
+      end
+
+      def parse_call_args
+        consume(TokenType::LPAREN, "Expected '('.")
+        args = [] # @type var args: Array[AST::expression]
+        args = parse_expression_list_until(TokenType::RPAREN) unless match?(TokenType::RPAREN)
+        consume(TokenType::RPAREN, "Expected ')' after arguments.")
+        args
+      end
+
+      def parse_string_literal
+        token = current_token
+        parse_error("Expected string literal.") unless [TokenType::STRING, TokenType::RAW_STRING].include?(token.type)
+        advance
+        value = token.value.to_s
+        AST::StringLiteral.new(value: value, location: token.location)
+      end
+
+      # :reek:FeatureEnvy
+      def parse_number_literal
+        token = consume(TokenType::NUMBER, "Expected number literal.")
+        value = token.value
+        if value.is_a?(String)
+          value = if value.match?(/[eE.]/)
+                    Float(value)
+                  else
+                    Integer(value, 10)
+                  end
+        end
+        AST::NumberLiteral.new(value: value, location: token.location)
+      end
+
+      def parse_boolean_literal
+        token_type = current_token.type
+        location = current_token.location
+        advance
+        AST::BooleanLiteral.new(value: token_type == TokenType::TRUE, location: location)
+      end
+
+      def parse_null_literal
+        token = advance
+        AST::NullLiteral.new(location: token.location)
+      end
+
+      def parse_unary_expression
+        token = advance
+        operator = UNARY_OPERATOR_MAP.fetch(token.type)
+        operand = parse_expression(Precedence::UNARY)
+        AST::UnaryOp.new(operator: operator, operand: operand, location: token.location)
+      end
+
+      def parse_parenthesized_expression
+        advance
+        expression = parse_expression
+        consume(TokenType::RPAREN, "Expected ')' after expression.")
+        expression
       end
 
       # :reek:TooManyStatements
@@ -175,6 +329,127 @@ module Ruby
 
       def record_error(error)
         errors << error
+      end
+
+      def parse_braced_literal
+        start = consume(TokenType::LBRACE)
+        return parse_set(start) if match?(TokenType::RBRACE)
+
+        first = parse_expression
+        if match?(TokenType::COLON)
+          parse_object(start, first)
+        else
+          parse_set(start, first)
+        end
+      end
+
+      def parse_identifier_expression
+        base = parse_variable
+        base = parse_reference(base) if match?(TokenType::DOT, TokenType::LBRACKET)
+        return base unless match?(TokenType::LPAREN)
+
+        AST::Call.new(name: base, args: parse_call_args, location: base.location)
+      end
+
+      def parse_variable
+        token = current_token
+        name = Helpers.variable_name_for(token)
+        advance
+        AST::Variable.new(name: name, location: token.location)
+      end
+
+      def parse_expression_list_until(end_token)
+        elements = [] # @type var elements: Array[AST::expression]
+        elements << parse_expression
+        append_expression_list(elements, end_token)
+        elements
+      end
+
+      def parse_expression_list_until_with_first(end_token, first_element)
+        elements = [] # @type var elements: Array[AST::expression]
+        elements << first_element
+        append_expression_list(elements, end_token)
+        elements
+      end
+
+      def parse_object_pair(key)
+        consume(TokenType::COLON, "Expected ':' after object key.")
+        value = parse_expression
+        [key, value]
+      end
+
+      def append_expression_list(elements, end_token)
+        while match?(TokenType::COMMA)
+          advance
+          break if match?(end_token)
+
+          elements << parse_expression
+        end
+      end
+
+      def append_object_pairs(pairs)
+        while match?(TokenType::COMMA)
+          advance
+          break if match?(TokenType::RBRACE)
+
+          pairs << parse_object_pair(parse_expression)
+        end
+      end
+
+      def parse_reference_path(path)
+        while match?(TokenType::DOT, TokenType::LBRACKET)
+          match?(TokenType::DOT) ? parse_dot_reference(path) : parse_bracket_reference(path)
+        end
+      end
+
+      def parse_dot_reference(path)
+        advance
+        segment_token = current_token
+        segment = parse_identifier(IdentifierContext.new(name: "reference", allowed_types: IDENTIFIER_TOKEN_TYPES))
+        path << AST::DotRefArg.new(value: segment, location: segment_token.location)
+      end
+
+      def parse_bracket_reference(path)
+        bracket_token = consume(TokenType::LBRACKET)
+        value = parse_expression
+        consume(TokenType::RBRACKET, "Expected ']' after reference path.")
+        path << AST::BracketRefArg.new(value: value, location: bracket_token.location)
+      end
+
+      def empty_set?(first_element)
+        match?(TokenType::RBRACE) && !first_element
+      end
+
+      def empty_set_literal(start_token)
+        advance
+        AST::SetLiteral.new(elements: [], location: start_token.location)
+      end
+
+      # Shared parsing helpers that do not depend on parser state.
+      module Helpers
+        def self.precedence_of(operator)
+          Precedence::BINARY.fetch(operator, Precedence::LOWEST)
+        end
+
+        def self.normalize_reference_base(base)
+          path = [] # @type var path: Array[AST::RefArg]
+          return [base, path] unless base.is_a?(AST::Reference)
+
+          reference = base # @type var reference: AST::Reference
+          [reference.base, reference.path.dup]
+        end
+
+        def self.variable_name_for(token)
+          token_type = token.type
+          case token_type
+          when TokenType::IDENT
+            token.value.to_s
+          when TokenType::UNDERSCORE
+            "_"
+          else
+            IDENTIFIER_TOKEN_NAMES.fetch(token_type) { token_type.to_s.downcase }
+          end
+        end
       end
 
       def safe_token_at(index)
--- spec/ruby/rego/parser_spec.rb
+++ spec/ruby/rego/parser_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Ruby::Rego::Parser do
     tokens = Ruby::Rego::Lexer.new(source).tokenize
     described_class.new(tokens).parse
   end
+
+  def parse_expression(source)
+    tokens = Ruby::Rego::Lexer.new(source).tokenize
+    described_class.new(tokens).send(:parse_expression)
+  end
+
   describe "#parse" do
     it "parses package declarations" do
       module_node = parse("package example.policy")
@@ -39,6 +45,111 @@ RSpec.describe Ruby::Rego::Parser do
       REGO
 
       expect { parse(source) }.to raise_error(Ruby::Rego::ParserError, /Rule parsing not implemented/)
+    end
+  end
+
+  describe "#parse_expression" do
+    it "parses literal types" do
+      string_literal = parse_expression("\"hello\\nworld\"")
+      raw_literal = parse_expression("`raw {value}`")
+      number_literal = parse_expression("42")
+      float_literal = parse_expression("3.14")
+      sci_literal = parse_expression("1e-3")
+      true_literal = parse_expression("true")
+      false_literal = parse_expression("false")
+      null_literal = parse_expression("null")
+
+      expect(string_literal).to be_a(Ruby::Rego::AST::StringLiteral)
+      expect(string_literal.value).to eq("hello\nworld")
+      expect(raw_literal).to be_a(Ruby::Rego::AST::StringLiteral)
+      expect(raw_literal.value).to eq("raw {value}")
+      expect(number_literal).to be_a(Ruby::Rego::AST::NumberLiteral)
+      expect(number_literal.value).to eq(42)
+      expect(float_literal.value).to eq(3.14)
+      expect(sci_literal.value).to be_within(1.0e-10).of(1.0e-3)
+      expect(true_literal).to be_a(Ruby::Rego::AST::BooleanLiteral)
+      expect(true_literal.value).to eq(true)
+      expect(false_literal).to be_a(Ruby::Rego::AST::BooleanLiteral)
+      expect(false_literal.value).to eq(false)
+      expect(null_literal).to be_a(Ruby::Rego::AST::NullLiteral)
+      expect(null_literal.value).to be_nil
+    end
+
+    it "parses unary expressions" do
+      not_expr = parse_expression("not true")
+      neg_expr = parse_expression("-1")
+
+      expect(not_expr).to be_a(Ruby::Rego::AST::UnaryOp)
+      expect(not_expr.operator).to eq(:not)
+      expect(not_expr.operand).to be_a(Ruby::Rego::AST::BooleanLiteral)
+
+      expect(neg_expr).to be_a(Ruby::Rego::AST::UnaryOp)
+      expect(neg_expr.operator).to eq(:minus)
+      expect(neg_expr.operand).to be_a(Ruby::Rego::AST::NumberLiteral)
+    end
+
+    it "parses binary operations with precedence" do
+      expr = parse_expression("1 + 2 * 3")
+
+      expect(expr).to be_a(Ruby::Rego::AST::BinaryOp)
+      expect(expr.operator).to eq(:plus)
+      expect(expr.left).to be_a(Ruby::Rego::AST::NumberLiteral)
+      expect(expr.right).to be_a(Ruby::Rego::AST::BinaryOp)
+      expect(expr.right.operator).to eq(:mult)
+    end
+
+    it "parses nested expressions with parentheses" do
+      expr = parse_expression("(1 + 2) * (3 - 4)")
+
+      expect(expr).to be_a(Ruby::Rego::AST::BinaryOp)
+      expect(expr.operator).to eq(:mult)
+      expect(expr.left.operator).to eq(:plus)
+      expect(expr.right.operator).to eq(:minus)
+    end
+
+    it "parses references with dot and bracket notation" do
+      expr = parse_expression("input.user.roles[0]")
+
+      expect(expr).to be_a(Ruby::Rego::AST::Reference)
+      expect(expr.base).to be_a(Ruby::Rego::AST::Variable)
+      expect(expr.base.name).to eq("input")
+      expect(expr.path.length).to eq(3)
+      expect(expr.path[0]).to be_a(Ruby::Rego::AST::DotRefArg)
+      expect(expr.path[0].value).to eq("user")
+      expect(expr.path[1]).to be_a(Ruby::Rego::AST::DotRefArg)
+      expect(expr.path[1].value).to eq("roles")
+      expect(expr.path[2]).to be_a(Ruby::Rego::AST::BracketRefArg)
+      expect(expr.path[2].value).to be_a(Ruby::Rego::AST::NumberLiteral)
+    end
+
+    it "parses composite literals" do
+      array = parse_expression("[1, true, \"a\"]")
+      object = parse_expression("{\"a\": 1, \"b\": 2}")
+      set = parse_expression("{1, 2, 3}")
+      empty_set = parse_expression("{}")
+
+      expect(array).to be_a(Ruby::Rego::AST::ArrayLiteral)
+      expect(array.elements.length).to eq(3)
+
+      expect(object).to be_a(Ruby::Rego::AST::ObjectLiteral)
+      expect(object.pairs.length).to eq(2)
+      expect(object.pairs[0][0]).to be_a(Ruby::Rego::AST::StringLiteral)
+
+      expect(set).to be_a(Ruby::Rego::AST::SetLiteral)
+      expect(set.elements.length).to eq(3)
+
+      expect(empty_set).to be_a(Ruby::Rego::AST::SetLiteral)
+      expect(empty_set.elements).to eq([])
+    end
+
+    it "parses function calls" do
+      expr = parse_expression("count([1, 2])")
+
+      expect(expr).to be_a(Ruby::Rego::AST::Call)
+      expect(expr.name).to be_a(Ruby::Rego::AST::Variable)
+      expect(expr.name.name).to eq("count")
+      expect(expr.args.length).to eq(1)
+      expect(expr.args.first).to be_a(Ruby::Rego::AST::ArrayLiteral)
     end
   end
 end
