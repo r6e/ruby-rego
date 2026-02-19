eafa2c368fdf074a269e2f69968db83f2eb3bae9
Rob Trame <3004637+r6e@users.noreply.github.com>
2026-02-18 17:44:37 -0700

    Align unifier object matching with OPA and expand destructuring tests (#8)

--- spec/ruby/rego/unifier_spec.rb
+++ spec/ruby/rego/unifier_spec.rb
@@ -40,6 +40,61 @@ RSpec.describe Ruby::Rego::Unifier do
     expect(results.first["y"].to_ruby).to eq(2)
   end
 
+  it "matches partial objects" do
+    pattern = Ruby::Rego::AST::ObjectLiteral.new(
+      pairs: [
+        [
+          Ruby::Rego::AST::StringLiteral.new(value: "a"),
+          Ruby::Rego::AST::Variable.new(name: "x")
+        ]
+      ]
+    )
+
+    results = unify(pattern, { "a" => 1, "b" => 2 })
+
+    expect(results.size).to eq(1)
+    expect(results.first["x"].to_ruby).to eq(1)
+  end
+
+  it "does not reuse object keys for multiple pairs" do
+    pattern = Ruby::Rego::AST::ObjectLiteral.new(
+      pairs: [
+        [Ruby::Rego::AST::Variable.new(name: "k1"), Ruby::Rego::AST::NumberLiteral.new(value: 1)],
+        [Ruby::Rego::AST::Variable.new(name: "k2"), Ruby::Rego::AST::NumberLiteral.new(value: 1)]
+      ]
+    )
+
+    results = unify(pattern, { "a" => 1 })
+
+    expect(results).to be_empty
+  end
+
+  it "does not allow repeated key variables in object patterns" do
+    pattern = Ruby::Rego::AST::ObjectLiteral.new(
+      pairs: [
+        [Ruby::Rego::AST::Variable.new(name: "k"), Ruby::Rego::AST::NumberLiteral.new(value: 1)],
+        [Ruby::Rego::AST::Variable.new(name: "k"), Ruby::Rego::AST::NumberLiteral.new(value: 1)]
+      ]
+    )
+
+    results = unify(pattern, { "a" => 1, "b" => 1 })
+
+    expect(results).to be_empty
+  end
+
+  it "does not leak bindings on mismatch" do
+    pattern = Ruby::Rego::AST::ArrayLiteral.new(
+      elements: [
+        Ruby::Rego::AST::Variable.new(name: "x"),
+        Ruby::Rego::AST::Variable.new(name: "y")
+      ]
+    )
+
+    results = unify(pattern, [1])
+
+    expect(results).to be_empty
+  end
+
   it "returns multiple solutions for key matches" do
     pattern = Ruby::Rego::AST::ObjectLiteral.new(
       pairs: [
